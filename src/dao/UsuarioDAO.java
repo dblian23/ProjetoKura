e73457afc11ff4b477fc50178318aeed8e20d0dc
mathocihara <matheusyukio.hocihara@gmail.com>
2025-12-08 10:13:13 -0300

    Fix: ajustes do DAO para compatibilidade com modelo atualizado (LocalDate)

--- src/dao/UsuarioDAO.java
+++ src/dao/UsuarioDAO.java
@@ -3,6 +3,8 @@ package dao;
 import connect.ConnectFactory;
 import Model.Usuario;
 import java.sql.*;
+import java.time.LocalDate;
+    
 
 public class UsuarioDAO {
 
@@ -15,8 +17,15 @@ public class UsuarioDAO {
             stmt.setString(1, usuario.getNome());
             stmt.setString(2, usuario.getEmail());
             stmt.setString(3, usuario.getSenha());
-            stmt.setString(4, usuario.getDataNascimento());
-
+//            stmt.setString(4, usuario.getDataNascimento());
+             LocalDate data = usuario.getDataNascimento();
+            if (data != null) {
+                stmt.setDate(4, Date.valueOf(data));
+            } else {
+                stmt.setNull(4, Types.DATE);
+            }
+            
+            
             stmt.execute();
             return true;
 
@@ -42,12 +51,16 @@ public class UsuarioDAO {
 
                 Usuario u = new Usuario();
 
-                u.setId(rs.getInt("id"));
+//                u.setId(rs.getInt("id"));
+                u.setIdUsuario(rs.getInt("idUsuario"));
                 u.setNome(rs.getString("nome"));
                 u.setEmail(rs.getString("email"));
                 u.setSenha(rs.getString("senha"));
-                u.setDataNascimento(rs.getString("dataNascimento"));
-
+//                u.setDataNascimento(rs.getString("dataNascimento"));
+                Date dataSql = rs.getDate("dataNascimento");
+                if (dataSql != null) {
+                    u.setDataNascimento(dataSql.toLocalDate());
+                }                
                 return u;
             }
 
