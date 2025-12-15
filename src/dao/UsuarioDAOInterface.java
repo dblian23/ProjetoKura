4d51dd227335e2f1ac074ba18aae62ecc046a8a6
Profissional <Profissional@10.0.71.88>
2025-12-15 01:04:28 -0300

    Adição dos metodos editar e excluir conta no UsuarioDao e UsuarioDAOInterface

--- src/dao/UsuarioDAOInterface.java
+++ src/dao/UsuarioDAOInterface.java
@@ -7,4 +7,8 @@ public interface UsuarioDAOInterface {
     boolean cadastrar(Usuario usuario);
 
     Usuario login(String email, String senha);
+    
+    boolean editar(Usuario usuario);
+
+    boolean excluir(int idUsuario);
 }
